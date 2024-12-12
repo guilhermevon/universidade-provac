6882212041601340dfdbf71ee15fd23163bc8ff0
unknown <CEMIG@NOTECEMIG25.provac.net>
2024-12-12 13:46:22 -0300

    alteracoes de db

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -6,47 +6,46 @@ const pool = new Pool({
   host: "192.168.0.232",
   database: "provac_producao",
   password: "Provac@2024",
-  port: "5432",
+  port: 5432,
 });
 
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err.stack);
-  } else {
-    console.log("Conexão bem-sucedida ao banco de dados!");
-    release(); // Libera a conexão de volta ao pool
+// Função para configurar o search_path para o schema educ_system
+const setSearchPath = async () => {
+  const client = await pool.connect();
+  try {
+    await client.query("SET search_path TO educ_system");
+    console.log("search_path configurado para educ_system.");
+  } catch (err) {
+    console.error("Erro ao configurar o search_path:", err.stack);
+  } finally {
+    client.release(); // Libera a conexão de volta ao pool
   }
-});
-
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err.stack);
-    return;
-  }
-
-  // Definindo o search_path para o schema educ_system
-  client.query("SET search_path TO educ_system", (err, res) => {
-    if (err) {
-      console.error("Erro ao configurar o search_path:", err.stack);
-    }
-    // Você agora pode fazer as consultas no schema educ_system
-    release();
-  });
-});
+};
 
 // Função para conectar ao banco de dados
 const connectDB = async () => {
   try {
     await pool.connect();
     console.log("Conexão com o banco de dados estabelecida com sucesso!");
+    // Configura o search_path logo após a conexão
+    await setSearchPath();
   } catch (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
+    console.error("Erro ao conectar ao banco de dados:", err.stack);
     process.exit(1); // Encerra o processo em caso de erro
   }
 };
 
-// Exporta o pool e a função de conexão
-module.exports = {
-  pool,
-  connectDB,
+// Exemplo de consulta
+const fetchData = async () => {
+  const client = await pool.connect();
+  try {
+    const result = await client.query("SELECT * FROM some_table");
+    console.log(result.rows);
+  } catch (err) {
+    console.error("Erro ao executar a consulta:", err.stack);
+  } finally {
+    client.release(); // Libera a conexão de volta ao pool
+  }
 };
+
+export { pool, connectDB, fetchData };
