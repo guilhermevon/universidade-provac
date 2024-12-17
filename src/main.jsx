81d55e708f43ef48aac5299d6328e18697262e77
unknown <CEMIG@NOTECEMIG25.provac.net>
2024-12-17 11:29:04 -0300

    ufaaa

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
-import Error from "./pages/error";
 
 const App = () => {
   return (
