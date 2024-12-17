18453be94582643da6d2f13bc145b6382f90aa08
unknown <CEMIG@NOTECEMIG25.provac.net>
2024-12-17 14:15:28 -0300

    asfafasfasfsfafsddds

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,6 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import logoProvac from "../../src/assets/logo_provac.png";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 200px;
+  color: white;
+  text-align: center; /* Centraliza o texto */
+
+  @media (max-width: 980px) {
+    width: 90%;
+    height: 70%;
+    text-align: center;
+  }
+
+  @media (max-width: 640px) {
+    width: 90%;
+    height: 80%;
+    padding: 1em;
+    text-align: center;
+  }
+
+  @media (max-width: 480px) {
+    width: 95%;
+    height: 85%;
+    padding: 0.5em;
+    text-align: center;
+  }
+`;
+
+const Logo = styled.img`
+  width: 150px; /* Ajuste o tamanho desejado da imagem */
+  height: auto;
+  margin-bottom: 20px; /* Espaço entre a imagem e o texto abaixo */
+`;
+
 const Text = styled.p`
   max-width: 600px; /* Define a largura máxima do parágrafo */
   line-height: 1.6; /* Espaçamento entre as linhas */
