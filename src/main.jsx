07db0605f2b564ec9193133b1140cb6da9660717
unknown <CEMIG@NOTECEMIG25.provac.net>
2024-12-17 11:09:21 -0300

    adoasksokdasokdsokaoskdasodksao

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Courses from "./pages/Courses";
-import Error from "./pages/error";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/courses" element={<Courses />} />
-      <Route
-        path="/error"
-        element={
-          <PrivateRoute>
-            <Error />
-          </PrivateRoute>
-        }
-      />
     </Routes>
   );
 };
